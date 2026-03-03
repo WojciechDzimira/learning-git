079cae273e64c8eb16dff0b550a6457aee8d73b4
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-03 13:36:22 +0100

    Add printing product counter

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ item_counter = 0
 for shop, items in shoping_list.items():
     print(f"Idę do {shop.capitalize()} i kupuje tam {[item.capitalize() for item in items]}.") #.join zamienione na capitalize()
     item_counter += len(items) 
+
+print(f"w sumie kupię: {item_counter} produktów.")
