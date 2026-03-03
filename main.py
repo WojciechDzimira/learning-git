732c61032b94e5d25f2878d5586842b880ec0660
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-03 13:27:15 +0100

    Add shoping list

---
+++ main.py
@@ -0,0 +1,8 @@
+#zadanie: pierwsze Samodzielne commity
+
+
+shoping_list = {
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "seler", "rukola"], 
+    "monopolowy": ["wino"]
+    }
