751d31f8f57193f44c27d674c298eb529a6b1bab
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-03 13:33:14 +0100

    Add for loop used for printing shops and intems

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ shoping_list = {
     "warzywniak": ["marchew", "seler", "rukola"], 
     "monopolowy": ["wino"]
     }
+item_counter = 0
+
+for shop, items in shoping_list.items():
+    print(f"Idę do {shop.capitalize()} i kupuje tam {[item.capitalize() for item in items]}.") #.join zamienione na capitalize()
+    item_counter += len(items) 
