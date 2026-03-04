495c7e1e46d0c327ba62623b4061ee76de19a442
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-04 08:29:45 +0100

    add new shop in shoping list

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-#zadanie: pierwsze Samodzielne commity
-
-
 shoping_list = {
     "piekarnia": ["chleb", "pączek", "bułki"],
-    "warzywniak": ["marchew", "seler", "rukola"], 
+    "warzywniak": ["marchew", "seler", "rukola"]
+    "cukiernia": ["krówki", "lizaki"], 
      }
 item_counter = 0
 
 for shop, items in shoping_list.items():
-    print(f"Idę do {shop.capitalize()} i kupuje tam {[item.capitalize() for item in items]}.") #.join zamienione na capitalize()
+    print(f"Idę do {shop.capitalize()} i kupuje tam {[item.capitalize() for item in items]}.") 
     item_counter += len(items) 
 
 print(f"w sumie kupię: {item_counter} produktów.")
 
-print("branch nowa")
+print("cześć, mentorze :)")
